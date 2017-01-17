6a0cecdb8c2136099d12534617ac02e7381ea94c
gamaliel <fabiogamaliel@gmail.com>
2017-01-17 17:17:11 -0400

    user register controller added

---
+++ app/user/user.controller.js
@@ -0,0 +1,40 @@
+import Boom from 'boom'
+import Common from '../config/common'
+import Config from '../config/config'
+import jwt from 'jsonwebtoken'
+import User from './user.model.js'
+
+
+// BLOQUE privateKey
+const privateKey = Config.key.privateKey
+const tokenExpiry = Config.key.tokenExpiry
+
+export default (req, res) => {
+	const passwordEncrypted = Common.encrypt(req.body.password)
+
+	User.findOne({ email: req.body.email })
+	.then((existingUser) => {
+		if(existingUser) {
+			return res.send(Boom.forbidden('please provide another user email'));
+		}
+
+		let user = new User({
+			email: req.body.email,
+			password: passwordEncrypted
+		})
+
+		const tokenData = {
+			id: user._id
+			email: user.email
+		}
+
+		const token = jwt.sign(tokenData, privateKey, { expiresIn: tokenExpiry })
+
+		Common.sentMailVerificationLink(user, token)
+
+		return res.send(Boom.forbidden('Please confirm your email id by clicking on link in email'))
+	})
+	.catch((err) => {
+		return res.send(Boom.forbidden(err))
+	})
+}
