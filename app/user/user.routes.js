89e07214495c8e22d00e7380b287ead50a6521f9
gamaliel <fabiogamaliel@gmail.com>
2017-01-20 16:56:43 -0400

    route verifyEmail with token params

--- app/user/user.routes.js
+++ app/user/user.routes.js
@@ -3,4 +3,7 @@ import UserController from './user.controller'
 export default (app) => {
 	app.route('/user/register')
 	   .post(UserController.register)
+	   
+	app.route('./user/verifyEmail/:token')
+	   .post(UserController.verifyEmail)
 }
