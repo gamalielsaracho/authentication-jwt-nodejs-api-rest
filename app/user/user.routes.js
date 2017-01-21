089461f1708efa1328ac1697699255071249ea36
gamaliel <fabiogamaliel@gmail.com>
2017-01-20 22:32:10 -0400

    route added to show all the users

--- app/user/user.routes.js
+++ app/user/user.routes.js
@@ -1,9 +1,13 @@
 import UserController from './user.controller'
 
 export default (app) => {
+	app.route('/users')
+	   .get(UserController.users)
+
 	app.route('/user/register')
 	   .post(UserController.register)
 	   
-	app.route('./user/verifyEmail/:token')
+	app.route('/user/verifyEmail/:token')
 	   .post(UserController.verifyEmail)
+
 }
